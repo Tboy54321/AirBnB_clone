44e12994653f6b59ac52aa4db82c4a3a919b0904
Tboy54321 <122861724+Tboy54321@users.noreply.github.com>
2023-08-13 21:56:51 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Importing the necessary classes"""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
+    """Class that store the review parameters"""
 
     place_id = ""
     user_id = ""
