062c69577dbd2030fa119b02cbc357420b21cdf8
Tboy54321 <122861724+Tboy54321@users.noreply.github.com>
2023-08-13 21:57:32 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Impoerting the necessary classes"""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
+    """Class that store the State Variables"""
 
     name = ""
