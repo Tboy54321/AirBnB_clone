e839fd343be3450d5853cf01290c30aa7b38e3b1
Tboy54321 <122861724+Tboy54321@users.noreply.github.com>
2023-08-13 21:55:25 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Importing the necessary classes"""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
+    """City class"""
 
     state_id = ""
     name = ""
